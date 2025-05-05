d0911b57154230cda839ecabca44490c969c3f19
Iambadasss <andonkindekov47@gmail.com>
2025-05-05 21:12:30 +0300

    Day 1: Number Guessing Game

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        int number2;
+
+        Console.WriteLine("Type a number between 1 and 100 (for the other player to guess):");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 1 || number > 100)
+        {
+            Console.WriteLine("Number is not between 1 and 100");
+            Environment.Exit(0);
+        }
+
+        do
+        {
+            Console.WriteLine("Guess the number:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number2 == number)
+            {
+                Console.WriteLine("You guessed the number!");
+            }
+            else if (number2 > number)
+            {
+                Console.WriteLine("The number is lower");
+            }
+            else
+            {
+                Console.WriteLine("The number is higher");
+            }
+
+        } while (number != number2);
+    }
+}
