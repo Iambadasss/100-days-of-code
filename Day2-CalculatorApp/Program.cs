66bb315931176c19fd6a779046f7eea9f3c2e465
Iambadasss <andonkindekov47@gmail.com>
2025-05-06 21:25:36 +0300

    Initial commit with Day1 and Day2 folders

---
+++ Day2-CalculatorApp/Program.cs
@@ -0,0 +1,61 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        string repeat;
+        do
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Choose an operation (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            double result = 0;
+            bool valid = true;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero!");
+                        valid = false;
+                    }
+                    else
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    valid = false;
+                    break;
+            }
+
+            if (valid)
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+
+            Console.Write("Do you want to calculate again? (yes/no): ");
+            repeat = Console.ReadLine().ToLower();
+
+        } while (repeat == "yes");
+
+        Console.WriteLine("Goodbye!");
+    }
+}
