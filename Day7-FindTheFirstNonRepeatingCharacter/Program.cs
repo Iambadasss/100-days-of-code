3d5dd6b15dea8a47a90356eeed863fec3f4d6440
Iambadasss <andonkindekov47@gmail.com>
2025-05-12 18:42:52 +0300

    Day 7: First Non-Repeating Character with Dictionary logic

---
+++ Day7-FindTheFirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,40 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a word or sentence: ");
+        string input = Console.ReadLine();
+
+        string cleanedInput = "";
+        foreach (char c in input.ToLower())
+        {
+            if (char.IsLetter(c))
+            {
+                cleanedInput += c;
+            }
+        }
+
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in cleanedInput)
+        {
+            if (charCount.ContainsKey(c))
+                charCount[c]++;
+            else
+                charCount[c] = 1;
+        }
+
+
+        foreach (char c in cleanedInput)
+        {
+            if (charCount[c] == 1)
+            {
+                Console.WriteLine($"First non-repeating character: {c}");
+                return;
+            }
+        }
+
+        Console.WriteLine("No unique character found.");
+    }
+}
